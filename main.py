afa6c7075589e5f7ea73e003d155a5d960f68fd5
leshocna <nick@trustedstructuresllc.com>
2025-07-27 19:59:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.get("/")
 def root():
     return {"status": "Backend is live", "module": "Project Tell No One"}
-
+    
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
