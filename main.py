bcc1b915d61dbe3ef443361db001399b396f0827
leshocna <nick@trustedstructuresllc.com>
2025-07-28 10:09:12 -0400

    Update main.py
    uvicorn.run     main.app

--- main.py
+++ main.py
@@ -94,4 +94,4 @@ def get_opportunities(
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main_v12:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
